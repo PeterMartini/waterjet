b858a995ceefc7d98d3889ff03d8f00c8c57909a
Peter Martini <PeterCMartini@GMail.com>
2017-03-15 23:23:37 -0400

    Day two, cleanup:
    Now that we've iterated on this a bit, let's do some cleanup.
    
    Some of this is just changing the order and grouping of variables,
    but the changes around the `one.png` and `two.png` files has a real
    impact.
    
    In the naïve implementation, we kept the tiles right next to where
    they were used, and would end up having to re-read it from disk
    every time.  Between the caching done by the file system and the
    fact that this computer has an SSD, there wasn't any noticeable
    penalty, and that's the point.  Penalties like that are often not
    noticed until far too late, and of course sometimes the penalties
    aren't worth spending the time to fix.
    
    Additionally, the fix introduces python dictionaries, how to store
    things in them, and how to pull them back out.

--- waterjet.py
+++ waterjet.py
@@ -7,27 +7,29 @@ class WaterJet:
 
     def __init__(self, width=640, height=640):
         pygame.init()
+        pygame.display.set_caption("Hi, Michael!")
+
         self.width = width
         self.height = height
         self.clicked = []
-        pygame.display.set_caption("Hi, Michael!")
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.tiles = {
+            "one": pygame.image.load('one.png').convert(),
+            "two": pygame.image.load('two.png').convert(),
+        }
 
         self.draw_background()
         pygame.display.update()
 
     def draw_background(self):
-        tile1 = pygame.image.load('one.png').convert()
         for x in range(0, 16):
             for y in range(0, 16):
-                self.screen.blit(tile1, (x * 40, y * 40))
+                self.screen.blit(self.tiles["one"], (x * 40, y * 40))
 
-        tile2 = pygame.image.load('two.png').convert()
         for pos in self.clicked:
-            self.screen.blit(tile2, pos)
+            self.screen.blit(self.tiles["two"], pos)
 
     def start(self):
-        tile = pygame.image.load('two.png').convert()
         while 1:
             for event in pygame.event.get():
                 print event
@@ -39,7 +41,7 @@ class WaterJet:
                 elif event.type == pygame.MOUSEMOTION:
                     self.draw_background()
                     position = (event.pos[0] - 20, event.pos[1] - 20)
-                    self.screen.blit(tile, position)
+                    self.screen.blit(self.tiles["two"], position)
                 pygame.display.update()
 
 if __name__ == "__main__":
